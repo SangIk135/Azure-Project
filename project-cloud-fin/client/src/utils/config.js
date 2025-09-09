805699a652c56c0fbc0c82ef705dfa8b660c5166
wjdduq <wjdduq4715@naver.com>
2025-09-09 14:50:26 +0900

    changed base url

--- project-cloud-fin/client/src/utils/config.js
+++ project-cloud-fin/client/src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = ""; // localhost
-// export const BASE_URL = `https://testbe1jo.azurewebsites.net`;
+// export const BASE_URL = ""; // localhost
+export const BASE_URL = `https://testbe1jo.azurewebsites.net`;
